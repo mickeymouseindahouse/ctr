22ab6a52f84abcacf6dffb21b460250fb885218a
lisa <lisavetpolo@gmail.com>
2025-01-27 17:43:25 +0200

    * undersampler updated * 0.57 beast result lisa 003

--- experiments/lisa003.py
+++ experiments/lisa003.py
@@ -1,4 +1,6 @@
 import os.path
+
+import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 from constants import getroot
 from data_loader.train_loader_session_splitter import TrainLoaderSessionSplitter
@@ -7,13 +9,14 @@ from pipeline.base_model_pipeline import BaseModelPipeline
 from preprocessor.ctr_ratio_label_preprocessor import RatioBasedLabelPreprocessor
 from preprocessor.fill_na_preprocessor import FillNaPreprocessor
 from preprocessor.undersampler import Undersampler
+from sklearn.ensemble import RandomForestClassifier
 
 RESULT_PATH = 'Lisa003'
 
 if __name__ == '__main__':
     data_loader = TrainLoaderSessionSplitter(result_path=RESULT_PATH,
                                              train_file=os.path.join(getroot(), "data/train_dataset_full.csv"),
-                                             preprocessing=BaseModelPipeline(steps=[Undersampler()]))
+                                             preprocessing=BaseModelPipeline(steps=[Undersampler(), FillNaPreprocessor()]))
     data_loader.load_data()
     X_train, X_test, y_train, y_test = data_loader.split_data()
     data_loader.dump_to_pickle()
@@ -23,8 +26,9 @@ if __name__ == '__main__':
             one_hot_features=['gender'],
             label_features=['product', 'campaign_id','webpage_id','product_category_1', 'product_category_2']
         ),
-        BaseModel(model=DecisionTreeClassifier(class_weight='balanced')), ],
-                                 grid_search_params={"BaseModel": {"model__min_samples_leaf": [2, 1, 3], }}, )
-    pipeline.grid_search(X_train, y_train)
+        BaseModel(model=RandomForestClassifier(n_estimators=100)), ],
+                                 grid_search_params={"BaseModel": {"model__min_samples_leaf": [ 1, 5, 10],
+                                                                   "model__n_estimators": np.logspace(1, 2.5, 10, dtype=int),  }}, )
+    pipeline.grid_search(X_train, y_train, cv=3, )
     pipeline.dump_to_pickle()
     pipeline.dump_results()
--- preprocessor/undersampler.py
+++ preprocessor/undersampler.py
@@ -8,7 +8,7 @@ class Undersampler(BasePreprocessor, TransformerMixin):
     """
     the preprocessor that keeps all is_click=1 rows and samples is_click=0
     """
-    def __init__(self, sample_number=None, return_splited=True,target_column = 'is_click', positive_value=1, negative_value=0):
+    def __init__(self, sample_number=None, return_splited=False,target_column = 'is_click', positive_value=1, negative_value=0, random_state=42):
         self.preprocessor_name = 'UnderSampler'
         self.target_column = target_column
         self.sample_number = sample_number
@@ -16,6 +16,7 @@ class Undersampler(BasePreprocessor, TransformerMixin):
         self.df = pd.DataFrame()
         self.positive_value = positive_value
         self.negative_value = negative_value
+        self.random_state = random_state
 
     def fit(self, X: pd.DataFrame, y: np.array):
         self.df = X.copy()
@@ -28,12 +29,15 @@ class Undersampler(BasePreprocessor, TransformerMixin):
         if self.sample_number is not None:
             only_0 = self.df.loc[self.df[self.target_column] == self.negative_value]
             if isinstance(self.sample_number, int):
-                only_0 = only_0.sample(frac=1).head(self.sample_number)
+                only_0 = only_0.sample(frac=1, random_state = self.random_state).head(self.sample_number)
             elif isinstance(self.sample_number, float):
-                only_0 = only_0.sample(frac=self.sample_number)
+                only_0 = only_0.sample(frac=self.sample_number,  random_state = self.random_state)
         else:
-            only_0 =self.df.loc[self.df[self.target_column]==self.negative_value].sample(frac=random.randint(1, 100)/100)
+            available_0s = self.df.loc[self.df[self.target_column]==self.negative_value]
+            available_length = min(len(only_1), available_0s.shape[0])
+            only_0 = pd.DataFrame(available_0s.sample(frac=1,random_state = self.random_state).head(available_length))
         transformed_df = pd.concat([only_1, only_0])
+        transformed_df = transformed_df.sample(frac=1)
         if self.return_splited:
             return transformed_df[[column for column in transformed_df.columns if column!= self.target_column]], transformed_df[self.target_column]
         else:
