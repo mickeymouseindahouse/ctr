1931582ae62acf221d41d9e470f31e6734e9124f
lisa <lisavetpolo@gmail.com>
2025-02-12 18:38:31 +0200

    * confusion matrices

--- experiments/train_data_confusion_matrixes.py
+++ experiments/train_data_confusion_matrixes.py
@@ -1,5 +1,4 @@
 import os.path
-from sklearn.metrics import confusion_matrix
 from constants import getroot
 from data_loader.train_loader_session_splitter import TrainLoaderSessionSplitter
 import matplotlib.pyplot as plt
@@ -7,7 +6,7 @@ from pipeline.base_model_pipeline import BaseModelPipeline
 import numpy as np
 import seaborn as sns
 from preprocessor.fill_na_preprocessor import FillNaPreprocessor
-
+from sklearn.metrics import confusion_matrix, accuracy_score, f1_score
 RESULT_PATH = 'train_data_confusion_matrixes.'
 
 if __name__ == '__main__':
@@ -29,21 +28,32 @@ if __name__ == '__main__':
     for product in X_test['product'].unique():
         df = X_test[X_test['product'] == product]
         print(product)
+
         cm = confusion_matrix(df['y_true'], df['y_pred'])
-        cm_percent = cm.astype('float') / cm.sum(axis=1, keepdims=True) * 100  # Convert to percentage
+
+        cm_percent = cm.astype('float') / cm.sum(axis=1, keepdims=True) * 100
+        cm_percent = np.nan_to_num(cm_percent, nan=0.0)
+        accuracy = accuracy_score(df['y_true'], df['y_pred'])
+        f1 = f1_score(df['y_true'], df['y_pred'], average='weighted')
+
+        labels = np.array([[f"{count} ({percent:.1f}%)" for count, percent in zip(row_count, row_percent)]
+                           for row_count, row_percent in zip(cm, cm_percent)])
+
+
         plt.figure(figsize=(6, 5))
-        sns.heatmap(cm_percent, annot=True, fmt=".2f", cmap="Blues", xticklabels=np.unique(y_test),
+        sns.heatmap(cm, annot=labels, fmt="", cmap="Blues", xticklabels=np.unique(y_test),
                     yticklabels=np.unique(y_test))
+
         plt.xlabel('Predicted Label')
         plt.ylabel('True Label')
-        plt.title(f'Confusion Matrix for Product: {product}')
+        plt.title(f'Confusion Matrix for Product: {product}\nAccuracy: {accuracy:.2f}, F1-score: {f1:.2f}')
+
 
         filename = os.path.join(save_dir, f'confusion_matrix_{product}.png')
         plt.savefig(filename, dpi=300, bbox_inches='tight')
         plt.close()
+
         print(f'Saved confusion matrix for {product} at {filename}')
-
-
 
 
 
